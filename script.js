9646734618166ffe414be13ab20b9e4ede747723
sanjayguwaju <sanjayguwaju@gmail.com>
2020-12-25 20:06:02 +0545

    codes clean up from css and script

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-var backdrop = document.querySelector('.backdrop');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
 toggleButton.addEventListener('click',function(){
     mobileNav.style.display = 'block';
-    // this.closeModal();
 });
-
-toggleButton.addEventListener('click',function(){
-    mobileNav.style.display = 'none';
-});
-
-// backdrop.addEventListener("click", closeModal);
-
-// function closeModal() {
-//     backdrop.style.display = "none";
-
-// };
