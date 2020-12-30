d4c9c1e352066db05eea7ddf14745366c7db3a7a
sanjayguwaju <sanjayguwaju@gmail.com>
2020-12-30 09:46:21 +0545

    added some script code for hamburger toggle

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var navIcon = document.getElementById('#nav-icon1');
 
-toggleButton.addEventListener('click',function(){
-    mobileNav.style.display = 'block';
+// toggleButton.addEventListener('click',function(){
+//     mobileNav.style.display = 'block';
+// });
+
+// Icon 1 
+
+$(document).ready(function(){
+	$('navIcon').click(function(){
+		$(this).toggleClass('open');
+	});
 });
